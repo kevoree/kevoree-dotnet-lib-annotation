e636c679d499cc66cd1cd59d08b6bc005e038285
Manuel Leduc <manuel.leduc@inria.fr>
2015-09-29 10:41:39 +0200

    Reflection on annotation moved from model generator to this specialised library

---
+++ AnnotationHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Org.Kevoree.Library.Annotation
+{
+    public class AnnotationHelper
+    {
+
+        private List<Tuple<Type, object>> filterByTypes(object[] types, Type[] pars)
+        {
+            var gootypes = new List<Tuple<Type, object>>();
+            foreach (object type in types)
+            {
+                foreach (var par in pars)
+                {
+                    if (type.GetType().Equals(par))
+                    {
+                        gootypes.Add(Tuple.Create(par, type));
+                    }
+                }
+            }
+            return gootypes;
+        }
+
+        private List<Tuple<Type, object>> GetTypeDefinitionSub(Type t, Type[] types)
+        {
+            return filterByTypes(t.GetCustomAttributes(true), types);
+        }
+
+        public Type GetTypeDefinition(Type filteredAssemblyTypes, Type[] expectedTypes)
+        {
+            return GetTypeDefinitionSub(filteredAssemblyTypes, expectedTypes)[0].Item1;
+        }
+
+        public bool FilterByAttribute(Type t, Type[] types)
+        {
+            return GetTypeDefinitionSub(t, types).Count > 0;
+        }
+    }
+}
